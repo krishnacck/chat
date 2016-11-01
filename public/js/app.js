a98adab09ae0024977510ad6d146625a495b4872
Chandu4221 <chndraja@gmail.com>
2016-11-01 18:10:45 +0530

    Browser can receive messages

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,17 @@
 var socket = io();
+var form = jQuery('#messageForm');
+var message = form.find("input[name=message]");
+var messagesList = jQuery('#messagesList');
 
 socket.on('connect',function(){
 	console.log('Front end started');
 });
 
 socket.on('message',function(message){
-	console.log(message.text);
+	messagesList.append('<p>'+message.text+'</p>');
 });
 
-var form = jQuery('#messageForm');
-var message = form.find("input[name=message]");
+
 
 form.on('submit',function (event) {
 	event.preventDefault();
--- server.js
+++ server.js
@@ -11,10 +11,9 @@ io.on('connection',function(socket){
 	
 	socket.on('message',function(message){
 		socket.broadcast.emit('message',message);
-		console.log(message.text);
 	});
 	socket.emit('message',{
-		text: 'Connected to the Server',
+		text: 'Connected Established for chatting',
 	});
 });
 
