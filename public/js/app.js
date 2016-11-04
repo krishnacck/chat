3738c956f83a3ba6cc232bcf06cd1973b9d6221a
Chandu4221 <chndraja@gmail.com>
2016-11-05 01:24:29 +0530

    Made Responsive

--- public/js/app.js
+++ public/js/app.js
@@ -10,11 +10,13 @@ socket.on('connect',function(){
 
 socket.on('message',function(message){
 	var timestamp = moment.utc(message.timestamp);
-	messagesList.append('<li><b class="badge">'+message.name+'</b> '+message.text+'<small>'+timestamp.local().format('h:mm a')+'</small></li>');	
-	
-	/* scroll to top automatically*/
+	messagesList.append('<li><b class="badge">'+message.name+'</b> '+message.text+'<small>'+timestamp.local().format('h:mm a')+'</small></li>');
+
+	/* scroll automatically*/
 	var msgDiv = document.getElementById('messages-container');
 	msgDiv.scrollTop = msgDiv.scrollHeight;
+	/*scroll automatically*/
+
 	});
 
 form.on('submit',function (event) {
